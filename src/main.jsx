e9929545dd3fc2525c5148c8bec2d4bd01888fe3
Orawee Chan <oraweee@gmail.com>
2024-06-26 13:55:43 -0600

    added animation and components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://d3bec92c26d42878c931ae7487c0719a@o4507493511266304.ingest.us.sentry.io/4507493513232384",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0,
+
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), sentryVitePlugin({
+    org: "jsm-qu",
+    project: "javascript-react"
+  })],
+
+  server: {
+    watch: {
+      usePolling: true, // Enable polling to detect file changes
+    },
+  },
+
+  build: {
+    sourcemap: true
+  }
+});
